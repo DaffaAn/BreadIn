d336fb1aa347e98178b1b0026ff490a9a6842484
DaffaAn <daffa19003@mail.unpad.ac.id>
2022-05-13 17:01:41 +0700

    push

--- ci-penjualan/application/config/autoload.php
+++ ci-penjualan/application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'checklogin', 'automaticnumber', 'terbilang');
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
